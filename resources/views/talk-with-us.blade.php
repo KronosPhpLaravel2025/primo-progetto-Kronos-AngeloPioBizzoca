b1c138ce4bbd8eee7423aaea5bb79d642e283393
Angelo Pio Bizzoca <angelobizzoca@gmail.com>
2025-11-18 11:50:35 +0100

    modifiche interfaccia grafica

--- resources/views/talk-with-us.blade.php
+++ resources/views/talk-with-us.blade.php
@@ -34,9 +34,16 @@
       </h3>
     </div>
     <div class="container d-flex justify-content-center align-items-center mt-3 flex-column">
-      <form class="w-50 p-4 bg-light rounded shadow-sm">
-        <div class="mb-3"><label for="exampleFormControlInput1" class="form-label">Email address</label><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></div>
-        <div class="mb-3"><label for="exampleFormControlTextarea1" class="form-label">Message</label><textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></div><button type="submit" class="btn btn-outline-secondary w-100">Submit</button>
+      <form class="w-50 p-4 bg-light rounded shadow">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-outline-secondary w-100">Submit</button>
       </form>
     </div>
     <hr class="m-0 border-secondary mt-5">
