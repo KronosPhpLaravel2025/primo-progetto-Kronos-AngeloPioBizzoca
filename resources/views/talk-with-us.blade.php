f1c100ea5a6a23d680762771373bc1c931098466
Angelo Pio Bizzoca <angelobizzoca@gmail.com>
2025-11-18 11:45:49 +0100

    modifiche interfaccia grafica

--- resources/views/talk-with-us.blade.php
+++ resources/views/talk-with-us.blade.php
@@ -28,13 +28,18 @@
         </div>
       </div>
     </nav>
-    <div class="container d-flex justify-content-center align-items-center min-vh-100 flex-column">
+    <div class="text-center pt-5 mb-2">
+      <h3 class="fw-semibold text-uppercase d-inline-block">
+      Get in touch with us
+      </h3>
+    </div>
+    <div class="container d-flex justify-content-center align-items-center mt-3 flex-column">
       <form class="w-50 p-4 bg-light rounded shadow-sm">
         <div class="mb-3"><label for="exampleFormControlInput1" class="form-label">Email address</label><input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></div>
         <div class="mb-3"><label for="exampleFormControlTextarea1" class="form-label">Message</label><textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></div><button type="submit" class="btn btn-outline-secondary w-100">Submit</button>
       </form>
     </div>
-    <hr class="m-0 border-secondary">
+    <hr class="m-0 border-secondary mt-5">
     <footer class="bg-body-tertiary text-center py-4">
       <div class="container">
         <div class="row justify-content-center gx-5">
