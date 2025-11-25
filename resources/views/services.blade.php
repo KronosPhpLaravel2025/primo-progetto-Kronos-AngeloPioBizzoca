d19263682805202ef504d6159dfec7aedf04fc67
angelopiobizzoca <angelobizzoca@gmail.com>
2025-11-25 10:54:41 +0100

    modifiche

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,7 +3,6 @@
       <div class="d-flex flex-column align-items-center pt-5 pb-5" style="max-width: 800px;">
         <h2 class="mb-4 mt-4 text-center text-uppercase">Services list</h2>
         <ul class="list-unstyled w-100">
-        
           <x-services_list :$services></x-services_list>
         </ul>
       </div>
--- resources/views/talk-with-us.blade.php
+++ resources/views/talk-with-us.blade.php
@@ -6,11 +6,11 @@
     <form class="w-50 p-4 bg-light rounded shadow">
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Insert here your email">
       </div>
       <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Insert here your message"></textarea>
       </div>
       <button type="submit" class="btn btn-outline-secondary w-100">Submit</button>
     </form>
